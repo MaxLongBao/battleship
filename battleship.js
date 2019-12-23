13dcb6eb4f1933070cb633356a3b11cfaba46fbb
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2019-12-24 00:16:26 +0100

    controller completed

--- battleship.js
+++ battleship.js
@@ -58,7 +58,12 @@ var controller = {
   processGuess: function(guess) {
     var location = parseGuess(guess);
     if (location) {
-
+      this.guesses++;
+      var hit = model.fire(location);
+      if (hit && model.shipsSunk === model.numShips) {
+        view.displayMessage("You sank all my battleships, in " + this.guesses
+          + " guesses");
+      }
     }
   }
 };
